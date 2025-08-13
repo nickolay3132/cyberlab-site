56478813b928e7cee92623664116482feb7ed4e3
nickolay3132 <nickolay3132@gmail.com>
2025-08-13 13:59:39 +0300

    Initial commit

---
+++ src/main/kotlin/store/technocyberlab/cyberlabsite/CyberlabSiteApplication.kt
@@ -0,0 +1,11 @@
+package store.technocyberlab.cyberlabsite
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CyberlabSiteApplication
+
+fun main(args: Array<String>) {
+	runApplication<CyberlabSiteApplication>(*args)
+}
---
+++ src/test/kotlin/store/technocyberlab/cyberlabsite/CyberlabSiteApplicationTests.kt
@@ -0,0 +1,13 @@
+package store.technocyberlab.cyberlabsite
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CyberlabSiteApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
